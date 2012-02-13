4b3102a82d154b34a530ab0cefe2f4a6593acc6d
Alejandro Gómez <alejandroogomez@gmail.com>
2012-02-13 10:10:33 +0100

    Initial commit

---
+++ src/main/com/dialelo/androidapp/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.dialelo.androidapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.
+
+public class HomeActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
